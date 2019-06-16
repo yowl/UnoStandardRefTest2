41e21488faeb88407ef5030a60bbedbf9ebd912d
scott <scott.waye@hubse.com>
2019-06-15 20:46:49 -0500

    add keydown support

--- UnoStandardRefTest2/UnoStandardRefTest2.Shared/MainPage.xaml.cs
+++ UnoStandardRefTest2/UnoStandardRefTest2.Shared/MainPage.xaml.cs
@@ -15,7 +15,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+#if __WASM__
+using Uno.Extensions;
+#endif
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace UnoStandardRefTest2
@@ -31,7 +33,40 @@ namespace UnoStandardRefTest2
             //            Items = Enumerable.Range(0, 2000).Select(i => i.ToString()).ToList();
             this.InitializeComponent();
             DataContext = this;
+#if __WASM__
+//            var keyDownHandler = new KeyEventHandler(OnMenuBarItemKeyDown);
+//            AddHandler(UIElement.KeyDownEvent, keyDownHandler, true);
+//            comboBox.AddHandler(UIElement.KeyDownEvent, keyDownHandler, true);
+            //            this.RegisterHtmlEventHandler("keydown", OnSimpleEvent);
+#endif
         }
+
+//        void OnMenuBarItemKeyDown(object sender, KeyRoutedEventArgs e)
+//        {
+//            if (e.Key == VirtualKey.Space)
+//            {
+//                comboBox.IsDropDownOpen = !comboBox.IsDropDownOpen;
+//            }
+//            if (e.Key == VirtualKey.Down)
+//            {
+//                if (comboBox.IsDropDownOpen)
+//                {
+//                    comboBox.SelectedIndex++;
+//                }
+//            }
+//        }
+
+#if __WASM__
+        private void OnSimpleEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("keydown2");
+            if (e == null)
+            {
+                Console.WriteLine("e is null " );
+            }
+            else Console.WriteLine("e is " + e.GetType());
+        }
+#endif
 
         public List<string> Items { get; set; }
     }
